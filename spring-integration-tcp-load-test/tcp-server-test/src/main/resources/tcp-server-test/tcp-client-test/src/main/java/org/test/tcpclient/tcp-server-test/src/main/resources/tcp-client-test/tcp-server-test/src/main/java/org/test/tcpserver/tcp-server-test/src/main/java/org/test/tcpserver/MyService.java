368610bc8246f202e9138dc02eccf4bd0d3a3d41
John Anderson <dtgjyhdty@yahoo.com>
2014-06-11 00:04:57 +0400

    Delete MyService.java

--- spring-integration-tcp-load-test/tcp-server-test/src/main/resources/tcp-server-test/tcp-client-test/src/main/java/org/test/tcpclient/tcp-server-test/src/main/resources/tcp-client-test/tcp-server-test/src/main/java/org/test/tcpserver/tcp-server-test/src/main/java/org/test/tcpserver/MyService.java
+++
@@ -1,8 +0,0 @@
-package org.test.tcpserver;
-
-public class MyService {
-	public void test(byte[] input) throws InterruptedException {
-		Thread.sleep(10);
-		System.out.println("Received: " + new String(input));
-	}
-}
